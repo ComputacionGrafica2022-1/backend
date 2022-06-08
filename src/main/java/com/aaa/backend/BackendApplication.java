89d92c5fe2596f39548e8f4e283ea53e74b47bac
Diego <dfsanchezme@gmail.com>
2022-06-08 10:49:50 -0500

    Deployed version added to allowed origins

--- src/main/java/com/aaa/backend/BackendApplication.java
+++ src/main/java/com/aaa/backend/BackendApplication.java
@@ -24,7 +24,7 @@ public class BackendApplication {
 					@Override
 					public void addCorsMappings(CorsRegistry registry) {
 							registry.addMapping("/**")
-									.allowedOrigins("http://localhost:3000")
+									.allowedOrigins("http://localhost:3000", "https://cg-aaa.herokuapp.com")
 									.allowedMethods("*");
 					}
 
